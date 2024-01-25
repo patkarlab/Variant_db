d5f166e2bb0f4a02d25c62d951becdc2fe0c2381
patkarlab <nvpatkar@gmail.com>
2024-01-25 13:03:12 +0530

    changed create_database.py and append_to_database.py

--- append_to_database.py
+++ append_to_database.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sqlite3
 import sys
 import os
@@ -18,18 +18,18 @@ def append_to_database(vcf_files):
                     gt_field = data[9].split(":")[0]
                     alt_num = gt_field.split('/').count('1')
 
-                    # Retrieving existing sample names and alt_num values
                     cursor.execute('SELECT SAMPLE_NAME, ALT_NUM FROM Variants WHERE CHROM=? AND POS=? AND REF=? AND ALT=?;', (chrom, pos, ref, alt))
                     existing_data = cursor.fetchone()
 
                     if existing_data:
                         existing_sample_ids, existing_alt_nums = existing_data
 
-                        # Appending new sample ID to the existing ones and counting the number of sample IDs
-                        new_sample_ids = f"{existing_sample_ids},{sample_id}"
-                        new_alt_nums = existing_alt_nums + alt_num
+                        if sample_id not in existing_sample_ids:
+                            # Appending new sample ID to the existing ones and counting the number of sample IDs
+                            new_sample_ids = f"{existing_sample_ids},{sample_id}"
+                            new_alt_nums = existing_alt_nums + alt_num
 
-                        cursor.execute('UPDATE Variants SET SAMPLE_NAME = ?, ALT_NUM = ? WHERE CHROM=? AND POS=? AND REF=? AND ALT=?;', (new_sample_ids, new_alt_nums, chrom, pos, ref, alt))
+                            cursor.execute('UPDATE Variants SET SAMPLE_NAME = ?, ALT_NUM = ? WHERE CHROM=? AND POS=? AND REF=? AND ALT=?;', (new_sample_ids, new_alt_nums, chrom, pos, ref, alt))
                     else:
                         cursor.execute('INSERT INTO Variants (CHROM, POS, REF, ALT, SAMPLE_NAME, ALT_NUM) VALUES (?, ?, ?, ?, ?, ?);', (chrom, pos, ref, alt, sample_id, alt_num))
 
@@ -40,7 +40,6 @@ def append_to_database(vcf_files):
         chrom, pos, ref, alt, sample_id, alt_num = variant
 
         # Calculating frequency
-
         num_samples = len(sample_id.split(','))  # Calculate num_samples for each variant
         frequency = alt_num / (2 * num_samples)
 
@@ -50,7 +49,7 @@ def append_to_database(vcf_files):
     conn.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         print("Usage: {} <vcf_file1> <vcf_file2> ...".format(sys.argv[0]))
         sys.exit(1)
 
